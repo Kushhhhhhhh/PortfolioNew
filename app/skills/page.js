37dc19b78e8d24fedf61b5a010e1cb8e847f8d1a
Kush <kush.73888@gmail.com>
2023-09-23 19:53:54 +0530

    basic setup done of skill part

--- app/skills/page.js
+++ app/skills/page.js
@@ -3,22 +3,24 @@ import React from 'react'
 import Navbar from '../components/Navbar.js';
 import Footer from '../components/Footer.js';
 
+
 const Skill = () => {
     const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Next.js', 'Node.js', 'Express.js', 'MongoDB'];
   
   return  <>
   <Navbar />
-
-      <main className='w-full min-h-screen'>
-      <div>
-      <h2>My Skill Set</h2>
-      <ul>
-        {skills.map((skill, index) => (
-          <li key={index}>{skill}</li>
-        ))}
-      </ul>
-    </div>
-      </main>
+  <main className="flex items-center justify-center w-full min-h-screen">
+  <div>
+    <h2 className="text-center text-slate-500">My Skill Set</h2>
+    <ul className="text-center">
+      {skills.map((skill, index) => (
+        <li key={index} className="py-2 transition ease-in-out duration-300 transform hover:scale-105">
+          {skill}
+        </li>
+      ))}
+    </ul>
+  </div>
+</main>
 
    <Footer />   
 </>
