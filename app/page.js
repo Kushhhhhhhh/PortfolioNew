7384a0bb47fd00ef279dc18155781a5c4855850f
Kush <kush.73888@gmail.com>
2023-09-23 18:24:38 +0530

    trying to found real error

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@
 import Link from 'next/link'
 import Navbar from './Navbar/page';
 import HomePage from './HomePage/page';
-import About from './about/page';
 
 export default function Home() {
   return (
