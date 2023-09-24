83b2ee199c9e9f3f920862cd6cf2a4c1a7c52f91
Kush <kush.73888@gmail.com>
2023-09-24 21:02:59 +0530

    vasic setup of projects , final commit of 24 sept

--- app/projects/page.js
+++ app/projects/page.js
@@ -3,11 +3,32 @@ import Navbar from '../components/Navbar.js';
 import Footer from '../components/Footer.js';
 
 const Projects = () => {
+
+   const data = [
+      {
+        project: {
+          title: "Kiks",
+          description: "My Social Media App"
+        }
+      },
+    ];
+
   return <>
  <Navbar />
 
     <main className='w-full min-h-screen'>
-         projects   
+    <div className="card card-side bg-base-100 shadow-xl">
+  <figure>
+   <img src="" alt="Movie"/>
+   </figure>
+  <div className="card-body">
+    <h2 className="card-title">{title}</h2>
+    <p>{description}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">{language}</button>
+    </div>
+  </div>
+</div>  
     </main>
 
  <Footer /> 
