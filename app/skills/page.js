e64e0c3adb866e7cc07547c2255d1ba64318b44a
Kush <kush.73888@gmail.com>
2023-09-24 18:55:59 +0530

    images not loading

--- app/skills/page.js
+++ app/skills/page.js
@@ -1,36 +1,60 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar.js';
 import Footer from '../components/Footer.js';
 
-const Skill = () => {
-
 const skillsData = [
-  { title: 'HTML', image: '../assets/HTML.png' },
-  { title: 'CSS', image: '' },
-  
+  { title: 'HTML', image: require('../assets/HTML.png').default },
+  { title: 'CSS', image: require('../assets/CSS.jpeg').default },
+  { title: 'JS', image: require('../assets/JS.jpeg').default },
+  { title: 'REACT', image: require('../assets/REACT.png').default },
+  { title: 'NEXTJS', image: require('../assets/NEXTJS.png').default },
+  { title: 'NODEJS', image: require('../assets/NODEJS.webp').default },
+  { title: 'EXPRESSJS', image: require('../assets/EXPRESSJS.jpeg').default },
+  { title: 'MONGODB', image: require('../assets/MONGODB.jpg').default },
 ];
-  
-  return  <>
-  <Navbar />
-  <main className="flex items-center justify-center w-full min-h-screen">
-  <div>
-    <h2 className="text-center text-slate-500">I'm proficient in working with :-</h2>
-    <div className="flex space-x-4 overflow-x-auto">
-          {skillsData.map((skill, index) => (
-            <div key={index} className="max-w-sm rounded overflow-hidden shadow-lg">
-              <img src={skill.image} alt={skill.title} className="w-full" />
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{skill.title}</div>
+
+const Skill = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % skillsData.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + skillsData.length) % skillsData.length);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <main className="flex mt-5 justify-center w-full min-h-screen">
+        <div>
+          <h2 className="text-center font-semibold text-slate-400 m-8">I'm proficient in working with :-</h2>
+          <div className="flex justify-center">
+            <div className="w-full md:w-48 p-2">
+              <div className="flex items-center flex-col">
+                <img src={skillsData[currentIndex].image} alt={skillsData[currentIndex].title} className="w-full h-auto" />
+                <div className="font-bold text-xl text-center mt-2">
+                  <p>{skillsData[currentIndex].title}</p>
+                </div>
               </div>
             </div>
-          ))}
+          </div>
+          <div className="flex justify-center mt-4">
+            <button onClick={prevSlide} className="mr-2" disabled={currentIndex === 0}>
+              Previous
+            </button>
+            <button onClick={nextSlide} className="ml-2" disabled={currentIndex === skillsData.length - 1}>
+              Next
+            </button>
+          </div>
         </div>
-  </div>
-</main>
+      </main>
 
-   <Footer />   
-</>
+      <Footer />
+    </>
+  );
 };
 
 export default Skill;
